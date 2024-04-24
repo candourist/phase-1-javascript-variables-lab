d9dd931a3c4263012d5451abdb9379841e61db68
candourist <sajadh.ismael@gmail.com>
2024-04-25 02:58:43 +0300

    Completed assignment + Found a bug too :D

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // Code your solution in this file!
-const companyName = 'Scuber';
-let mostProfitableNeighborhood = "Chelsea";
-let companyCeo = "Susan Smith";
+// const companyName = 'Scuber';
+// let mostProfitableNeighborhood = "Chelsea";
+// let companyCeo = "Susan Smith";
+// $ git commit -m "complete lab"
+let companyName = 'Scuber';
+//There's a bug there, lol! 
+
+let companyCeo = 'Susan Smith';
+let mostProfitableNeighborhood = 'Chelsea';
+
+it('is set as Scuber', function () {
+    expect(companyName).to.equal('Scuber');
+  });
+
+ 
